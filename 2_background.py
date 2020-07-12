1fe75253c02400c35762801e4f9c07c0d45b8e98
zuikeaidetimo <50833001+zuikeaidetimo@users.noreply.github.com>
2020-07-12 14:27:32 +0900

    Add files via upload

---
+++ 2_background.py
@@ -0,0 +1,29 @@
+import pygame
+
+# 초기화 (반드시 필요)
+pygame.init() 
+
+# 화면 크기 설정
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Teemo's Game") # 게임 이름
+
+# 배경 이미지 불러오기
+background = pygame.image.load("C:/Users/DHJY/PycharmProjects/Pygame/pygame_basic/background.png")
+
+# 이벤트 루프
+running = True # 게임이 진행중인가?
+while running:
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
+            running = False # 게임이 진행중이 아님
+    
+    screen.blit(background, (0, 0)) # 배경 그리기
+
+    pygame.display.update()
+
+# pygame 종료
+pygame.quit()
